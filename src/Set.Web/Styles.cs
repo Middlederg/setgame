a13ca6cbe7b3c56b1623f78333a52070216982f7
jcl86 <jorgecano86@gmail.com>
2021-11-27 00:32:58 +0100

    Refactoring ui 2

--- src/Set.Web/Styles.cs
+++ src/Set.Web/Styles.cs
@@ -2,6 +2,15 @@
 {
     public static class Styles
     {
+        public const string Background = "gray-100";
+        public const string BackgroundDarker = "gray-200";
+        public const string Text = "gray-700";
+        public const string PrimaryColorLigther = "gray-600";
+        public const string PrimaryColor = "gray-700";
+        public const string PrimaryColorDarker = "gray-800";
         public const string Link = "text-xs font-bold text-indigo-600 hover:text-indigo-800 lg:text-base";
+        public static string Title = $"text-{PrimaryColor} font-semibold uppercase text-xl xl:text-4xl";
+        public static string SubTitle = $"text-sm text-{PrimaryColor} tracking-widest lg:text-lg";
+        public static string Container = $"h-full flex justify-center md:pt-20 p-8 bg-{Background}";
     }
 }
